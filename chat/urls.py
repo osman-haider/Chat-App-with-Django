0668c4c937e5bc10774c769548efb5a76d325e89
Usman Haider <gusmang525@gmail.com>
2024-03-10 20:24:03 +0500

    add url for room app

--- chat/urls.py
+++ chat/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('rooms', include('room.urls')),
+    path('rooms/', include('room.urls')),
     path('admin/', admin.site.urls),
 ]
